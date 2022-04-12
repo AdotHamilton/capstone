d68f08fa7e1e3f38b32af3abd171468f0e20da39
Austin Hamilton <76780774+AdotHamilton@users.noreply.github.com>
2022-04-12 17:02:33 -0500

    Update FileController.java

--- src/main/java/com/teksystems/capstone/controller/FileController.java
+++ src/main/java/com/teksystems/capstone/controller/FileController.java
@@ -24,26 +24,27 @@ public class FileController {
     private FileStorageService fileStorageService;
 
     @GetMapping("/users/{id}/{filename:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String filename, @PathVariable Long id, HttpServletRequest request){
-        Resource resource = fileStorageService.loadFileAsResource(filename, "users/" + id);
+    public ResponseEntity<Resource> getPfp(@PathVariable String filename, @PathVariable Long id, HttpServletRequest request){
+        Resource resource = fileStorageService.loadFileAsResource(filename, "users/" + id); // find file in system, C:/uploads/ + users/{id}/ + filename 
 
         String contentType = null;
 
         try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath()); // I dont even know
         } catch (IOException e) {
            log.info("Couldnt determine filetype");
         }
 
        if(contentType == null){
            contentType = "application/octet-stream";
-       }
-
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
+       } 
+        // probably add some validation not to return a file unless it is a JPG, PNG etc...
+        String mediaType = MediaType.parseMediaType(contentType);
+        return ResponseEntity.ok().contentType(mediaType).body(resource); // return file resource
     }
     @GetMapping("/posts/{id}/{filename:.+}")
     public ResponseEntity<Resource> getPost(@PathVariable String filename, @PathVariable Long id, HttpServletRequest request){
-        Resource resource = fileStorageService.loadFileAsResource(filename, "posts/" + id);
+        Resource resource = fileStorageService.loadFileAsResource(filename, "posts/" + id); // load file from C:/uploads/posts/{id}/filename
 
         String contentType = null;
 
